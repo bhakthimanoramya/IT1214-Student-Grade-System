4a47eee807ad191178391468e42e5551083612ba
bhakthimanoramya <bhakthimanoramya21@gmail.com>
2026-06-09 22:04:20 +0530

    Created a student class

---
+++ src/Student.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+//student class
+class Student{
+	private int studentID;
+	private String studentName;
+	private double marks;
+	
+	Student(int studentID,String studentName,double marks){
+		
+	this.studentID=studentID;
+	this.studentName=studentName;
+	this.marks=marks;	
+	}
+	
+	
+	public int getStudentID(){
+		return studentID;
+	}
+	public String getStudentName(){
+		return studentName;
+	}
+	public double getMarks(){
+		return marks;
+	}
+	
+	
+	public void setStudentID(int studentID){
+		this.studentID=studentID;
+	}
+	public void setStudentName(String studentName){
+		this.studentName=studentName;
+	}
+	public void setMarks(double marks){
+		this.marks=marks;
+	}
+}
